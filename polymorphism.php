8cf596541cbbbec7a4633e8c89c8a41b3640c1d4
Stephanie <stephgithinji67@gmail.com>
2019-03-22 14:04:28 +0300

    update of polymorphism

--- polymorphism.php
+++ polymorphism.php
@@ -24,3 +24,34 @@ $author=new Author();
 $author->calculateScore();
 $editor=new Editor();
 $editor->calculateScore();
+?>
+
+
+<?php
+interface Philosophers
+{
+
+    public function calculateScore();
+}
+class Reader implements Philosophers
+{
+    public $article=10;
+    public $read=15;
+    public function calculateScore()
+    {
+        echo "<br><br>The Reader's score is:<br><br>" . $this->article * $this->read;
+    }
+}
+class Writer implements Philosophers
+{
+    public $article=12;
+    public $written=23;
+    public function calculateScore()
+    {
+        echo "<br><br>The Writer's  score is:<br><br>" . $this->article * $this->written;
+    }
+}
+$reader=new Reader();
+$reader->calculateScore();
+$writer=new Writer();
+$writer->calculateScore();
